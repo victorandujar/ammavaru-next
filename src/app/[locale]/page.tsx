3d8bb96f6aa5a8ea18b0fd1e68d406f5120291e8
victorandujar <victorandujarmurcia@gmail.com>
2024-12-12 10:56:01 +0100

    Add first implementation for Methodology section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,13 @@
 import AboutSection from "../components/AboutSection/AboutSection";
 import HeroSection from "../components/HeroSection/HeroSection";
+import MethodologySection from "../components/MethodologySection/MethodologySection";
 
 export default function Home() {
   return (
     <div className="flex w-3/4 flex-col gap-40">
       <HeroSection />
       <AboutSection />
+      <MethodologySection />
     </div>
   );
 }
