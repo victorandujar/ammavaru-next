74aaa69e522b11e33e6c0455299f1165fd010b94
victorandujar <victorandujarmurcia@gmail.com>
2024-12-12 13:32:50 +0100

    Create ServiceSection compo + add its styles & functionality

---
+++ src/app/components/ServicesSection/ServicesSection.tsx
@@ -0,0 +1,117 @@
+"use client";
+
+import { useRef, useState, useEffect } from "react";
+import { useTranslations } from "next-intl";
+import { serviceSections } from "./utils/serviceSections";
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
+import { FaCircle } from "react-icons/fa";
+
+const ServicesSection = (): React.ReactElement => {
+  const t = useTranslations("Services");
+  const containerRef = useRef<HTMLUListElement>(null);
+
+  const [canScrollLeft, setCanScrollLeft] = useState(false);
+  const [canScrollRight, setCanScrollRight] = useState(true);
+
+  const handleScrollRight = () => {
+    if (containerRef.current) {
+      const container = containerRef.current;
+      const scrollAmount = container.firstElementChild?.clientWidth || 0;
+      container.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    }
+  };
+
+  const handleScrollLeft = () => {
+    if (containerRef.current) {
+      const container = containerRef.current;
+      const scrollAmount = container.firstElementChild?.clientWidth || 0;
+      container.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+    }
+  };
+
+  const updateScrollButtons = () => {
+    if (containerRef.current) {
+      const container = containerRef.current;
+      setCanScrollLeft(container.scrollLeft > 0);
+      setCanScrollRight(
+        container.scrollLeft + container.offsetWidth < container.scrollWidth,
+      );
+    }
+  };
+
+  useEffect(() => {
+    updateScrollButtons();
+    const container = containerRef.current;
+    if (container) {
+      container.addEventListener("scroll", updateScrollButtons);
+      return () => container.removeEventListener("scroll", updateScrollButtons);
+    }
+  }, []);
+
+  return (
+    <section className="relative flex flex-col gap-20">
+      <h5 className="font-bold text-mainColor">{t("title")}</h5>
+      <div className="relative">
+        <ul
+          ref={containerRef}
+          className="flex gap-32 overflow-x-auto scrollbar-hide"
+          style={{
+            scrollSnapType: "x mandatory",
+            paddingBottom: "10px",
+          }}
+        >
+          {serviceSections.map((section) => (
+            <li
+              key={section.id}
+              className="w-96 flex-shrink-0"
+              style={{
+                scrollSnapAlign: "start",
+              }}
+            >
+              <article className="flex flex-col gap-10">
+                <span className="text-mainColor h-10">{t(section.title)}</span>
+                <span className="text-mss">{t(section.description)}</span>
+                <ul className="list-disc flex flex-col gap-3  h-40">
+                  {section.details.map((detail) => (
+                    <li
+                      key={detail.id}
+                      className="leading-5 text-mss flex  gap-4"
+                    >
+                      <div className="w-1 pt-2">
+                        <FaCircle size={5} />
+                      </div>
+                      {t(detail.name)}
+                    </li>
+                  ))}
+                </ul>
+                <div className="flex w-full justify-end items-end">
+                  <button className="text-ms text-mainColor border-solid border-1 border-mainColor rounded-3xl px-5 py-2 hover:bg-mainColor hover:text-white hover:font-bold">
+                    {t("button-text")}
+                  </button>
+                </div>
+              </article>
+            </li>
+          ))}
+        </ul>
+        {canScrollLeft && (
+          <button
+            onClick={handleScrollLeft}
+            className="absolute top-1/2 -left-40 transform -translate-y-1/2  text-white p-2 rounded-full shadow-md"
+          >
+            <BsChevronLeft color={"#888aff"} size={40} />
+          </button>
+        )}
+        {canScrollRight && (
+          <button
+            onClick={handleScrollRight}
+            className="absolute top-1/2 -right-40 transform -translate-y-1/2 text-white p-2 rounded-full shadow-md"
+          >
+            <BsChevronRight color={"#888aff"} size={40} />
+          </button>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default ServicesSection;
---
+++ src/app/components/ServicesSection/utils/serviceSections.ts
@@ -0,0 +1,96 @@
+import { ServiceInterface } from "@/app/interfaces/Services";
+
+export const serviceSections: ServiceInterface[] = [
+  {
+    id: 1,
+    title: "first-service.title",
+    description: "first-service.description",
+    details: [
+      {
+        id: 1.1,
+        name: "first-service.first-detail",
+      },
+      {
+        id: 1.2,
+        name: "first-service.second-detail",
+      },
+      {
+        id: 1.3,
+        name: "first-service.third-detail",
+      },
+      {
+        id: 1.4,
+        name: "first-service.fourth-detail",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "second-service.title",
+    description: "second-service.description",
+    details: [
+      {
+        id: 2.1,
+        name: "second-service.first-detail",
+      },
+      {
+        id: 2.2,
+        name: "second-service.second-detail",
+      },
+      {
+        id: 2.3,
+        name: "second-service.third-detail",
+      },
+      {
+        id: 2.4,
+        name: "second-service.fourth-detail",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "third-service.title",
+    description: "third-service.description",
+    details: [
+      {
+        id: 3.1,
+        name: "third-service.first-detail",
+      },
+      {
+        id: 3.2,
+        name: "third-service.second-detail",
+      },
+      {
+        id: 3.3,
+        name: "third-service.third-detail",
+      },
+      {
+        id: 3.4,
+        name: "third-service.fourth-detail",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "fourth-service.title",
+    description: "fourth-service.description",
+    details: [
+      {
+        id: 4.1,
+        name: "fourth-service.first-detail",
+      },
+      {
+        id: 4.2,
+        name: "fourth-service.second-detail",
+      },
+      {
+        id: 4.3,
+        name: "fourth-service.third-detail",
+      },
+      {
+        id: 4.4,
+        name: "fourth-service.fourth-detail",
+      },
+    ],
+  },
+];
